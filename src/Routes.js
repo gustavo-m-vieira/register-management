f5b28438fbcbdc41e5aea3acc2c3a23875b6ef88
Gustavo <gustavo@bgcbrasil.com.br>
2021-07-25 18:26:22 -0300

    created not found page

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
+// PAGES
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   );
