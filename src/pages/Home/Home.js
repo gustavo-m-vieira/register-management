7dd2cd3cb02d1b8d66f7a653f87ecda888d34cfd
Gustavo <gustavo@bgcbrasil.com.br>
2021-07-25 18:21:58 -0300

    created landing page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Register Management</h1>
+        <p className="text-muted">A simple app to manager registers</p>
+      </div>
+    </div>
+  );
+}
